9e439bd6675b699758438b562f65da99e566e26e
welbstercczdev <welbstercczdev@gmail.com>
2025-12-19 09:14:54 -0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/censopet-sjc/sw.js', { scope: '/censopet-sjc/' })})}
